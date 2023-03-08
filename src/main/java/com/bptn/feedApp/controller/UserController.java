066f8647590f7251675e393dc3c02ba6896d0f7f
techbyruke <100393943+techbyruke@users.noreply.github.com>
2023-03-07 21:02:14 -0500

    commit after UserService

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.bptn.feedApp.controller;
-
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.bptn.feedApp.controller.jdbc.UserBean;
+import com.bptn.feedApp.service.UserService;
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.sql.Timestamp;
@@ -32,6 +31,38 @@ public class UserController {
 	
 	}
 	
+	@GetMapping("/")
+	public List<UserBean> listUsers() {
+		logger.debug("The listUsers() method was invoked!");
+		return this.userService.listUsers();
+	}
 	
+	@GetMapping("/{userName}")
+	public UserBean findByUsername(@PathVariable String userName) {
+		logger.debug("The findByUsername() method was invoked!, userName={}", userName);
+		return this.userService.findByUsername(userName);
+	}
 	
+	@GetMapping("/{first}/{last}/{username}/{password}/{phone}/{emailId}")
+	public String createUser( @PathVariable String first,@PathVariable String last,@PathVariable String username,@PathVariable String password, 
+			@PathVariable String phone, @PathVariable String emailId) {
+		UserBean user = new UserBean();
+		
+		user.setUser_first_name(first);
+		user.setUser_last_name(last);
+		user.setUser_handle(username);
+		user.setUser_password(password);
+		user.setUser_phone_number(phone);
+		user.setUser_email(emailId);
+		user.setEmail_verified(false);
+		user.setDate_created(Timestamp.from(Instant.now()));
+				
+		logger.debug("The createUser() method was invoked!, user={}", user.toString());
+				
+		this.userService.createUser(user);
+				
+		return "User Created Successfully";
+}
+	
+
 }
---
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -0,0 +1,35 @@
+package com.bptn.feedApp.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedApp.controller.jdbc.UserBean;
+import com.bptn.feedApp.controller.jdbc.UserDao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserDao userDao;
+	
+	
+	public List<UserBean> listUsers() {
+		return this.userDao.listUsers();
+			
+	}
+	
+	public UserBean findByUsername(String userName) {
+		return this.userDao.findByUsername(userName);
+	}
+	
+	public void createUser(UserBean user) {
+		this.userDao.createUser(user);
+	}
+	
+	
+}
+
