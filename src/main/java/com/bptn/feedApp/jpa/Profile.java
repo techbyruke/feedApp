cee1c8266ac24f0bd894f125f0d967c6ba75bfb5
techbyruke <techbyruke@gmail.com>
2023-03-09 19:47:01 -0500

    commit after Profile.java

--- src/main/java/com/bptn/feedApp/jpa/Profile.java
+++ src/main/java/com/bptn/feedApp/jpa/Profile.java
@@ -1,5 +1,127 @@
 package com.bptn.feedApp.jpa;
 
-public class Profile {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+@Table(name="\"Profile\"")
+public class Profile implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="\"profileId\"")
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private Integer profileId;
+	
+	
+	private String bio;
+	private String city;
+	private String country;
+	private String headline;
+	private String picture;
+	
+	
+	@OneToOne
+	@JsonIgnore
+	@JoinColumn(name="\"userId\"")
+	private User user;
+	
+	
+	public Profile() {
+	}
+
+
+	public Integer getProfileId() {
+		return profileId;
+	}
+
+
+	public void setProfileId(Integer profileId) {
+		this.profileId = profileId;
+	}
+
+
+	public String getBio() {
+		return bio;
+	}
+
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getHeadline() {
+		return headline;
+	}
+
+
+	public void setHeadline(String headline) {
+		this.headline = headline;
+	}
+
+
+	public String getPicture() {
+		return picture;
+	}
+
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Profile [profileId=" + profileId + ", bio=" + bio + ", city=" + city + ", country=" + country
+				+ ", headline=" + headline + ", picture=" + picture + ", user=" + user + "]";
+	}
+	
+		
 }
