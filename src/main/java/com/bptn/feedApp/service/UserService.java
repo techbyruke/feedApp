921b3df7bc491747172ca4c20210a819e69bca0e
techbyruke <techbyruke@gmail.com>
2023-03-09 20:34:53 -0500

    commit after UserService.java

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -1,9 +1,12 @@
 package com.bptn.feedApp.service;
 import org.springframework.stereotype.Service;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bptn.feedApp.repository.UserRepository;
+
 import com.bptn.feedApp.jpa.User;
 
 
@@ -23,11 +26,29 @@ public class UserService {
 		return this.userRepository.findByUsername(username);
 	}
 	
+	public void deleteUserById(Integer userId) {
+		userRepository.deleteById(userId);
+	}
+
 	public void createUser(User user) {
 		this.userRepository.save(user);
 	}
 	
-	
+public User signup(User user){
+		
+		user.setUserName(user.getUserName().toLowerCase());
+		user.setEmailId(user.getEmailId().toLowerCase());
+		
+		user.setEmailVerified(false);
+		
+		user.setCreatedOn(Timestamp.from(Instant.now()));
+		
+		this.userRepository.save(user);
+		
+		return user;
+		
+	}
+
 }
 
 
