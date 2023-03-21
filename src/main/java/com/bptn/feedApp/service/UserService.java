6f4dd25371dcdfef60e83bd76b0222cbf1078b19
techbyruke <techbyruke@gmail.com>
2023-03-21 14:39:04 -0400

    commit after UserService.java

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -6,15 +6,24 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bptn.feedApp.exception.domain.EmailExistException;
+import com.bptn.feedApp.exception.domain.EmailNotVerifiedException;
 import com.bptn.feedApp.exception.domain.UserNotFoundException;
 import com.bptn.feedApp.exception.domain.UsernameExistException;
 import com.bptn.feedApp.jpa.User;
+import com.bptn.feedApp.provider.ResourceProvider;
 import com.bptn.feedApp.repository.UserRepository;
+import com.bptn.feedApp.security.JwtService;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import org.springframework.http.HttpHeaders;
 
 @Service
 public class UserService {
@@ -28,6 +37,14 @@ public class UserService {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	JwtService jwtService;
+
+	@Autowired
+	ResourceProvider provider;
 	
 	public List<User> listUsers() {
 		return this.userRepository.findAll();		
@@ -58,6 +75,12 @@ public class UserService {
 		this.userRepository.save(user);
 }
 
+	public HttpHeaders generateJwtHeader(String username) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username,this.provider.getJwtExpiration()));
+
+		return headers;
+	}
 	
 	private void validateUsernameAndEmail(String username, String emailId) {
 
@@ -71,6 +94,19 @@ public class UserService {
 
 }
 	
+	
+	private static User isEmailVerified(User user) {
+		 
+		if (user.getEmailVerified().equals(false)) {
+	        throw new EmailNotVerifiedException(String.format("Email requires verification, %s", user.getEmailId()));
+	    }	
+			
+	    return user;
+	}
+	
+	private Authentication authenticate(String username, String password) {
+		return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+}
 	
 	public User signup(User user) {
 
@@ -89,6 +125,13 @@ public class UserService {
 		return user;
 
 }
+	public User authenticate(User user) {
 
+		/* Spring Security Authentication. */
+		this.authenticate(user.getUsername(), user.getPassword());
+
+		/* Get User from the DB. */
+		return this.userRepository.findByUsername(user.getUsername()).map(UserService::isEmailVerified).get();
+	}
 		
 }
