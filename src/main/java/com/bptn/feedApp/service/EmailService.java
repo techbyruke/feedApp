a62f13a385cb5665ddec3b82ef0ebb595a1aa1c6
techbyruke <techbyruke@gmail.com>
2023-03-21 15:34:45 -0400

    commit after EmailService.java

--- src/main/java/com/bptn/feedApp/service/EmailService.java
+++ src/main/java/com/bptn/feedApp/service/EmailService.java
@@ -35,6 +35,13 @@ public class EmailService {
 				String.format("Welcome %s %s", user.getFirstName(), user.getLastName()),
 				this.provider.getClientVerifyExpiration());
 	}
+	
+	
+	@Async
+	public void sendResetPasswordEmail(User user) {
+			
+		this.sendEmail(user, this.provider.getClientResetParam(), "reset_password", "Reset your password", this.provider.getClientResetExpiration());
+	}	
 
 	private void sendEmail(User user, String clientParam, String templateName, String emailSubject, long expiration) {
 		try {
