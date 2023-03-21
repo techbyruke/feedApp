e5ff939ff9a79b6984b5ba6e03dfebe7644bbf40
techbyruke <techbyruke@gmail.com>
2023-03-21 00:06:01 -0400

    commit after CustomAuthEntryPoint.java

---
+++ src/main/java/com/bptn/feedApp/filter/CustomAuthEntryPoint.java
@@ -0,0 +1,29 @@
+package com.bptn.feedApp.filter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class CustomAuthEntryPoint implements AuthenticationEntryPoint {
+	
+	@Autowired
+	@Qualifier("handlerExceptionResolver")
+	HandlerExceptionResolver resolver;
+
+	@Override
+	public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException ex) throws IOException {
+	    	
+	    	this.resolver.resolveException(req, res, null, ex);
+	}
+
+}
