498b1219499f28b8037da61102c2549ccda52b53
techbyruke <techbyruke@gmail.com>
2023-03-21 15:10:02 -0400

    commit after UserController.java

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -14,10 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import static org.springframework.http.HttpStatus.OK;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
  
 import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.service.UserService;
- 
+
+
+
+@CrossOrigin(exposedHeaders = "Authorization")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -26,11 +34,30 @@ public class UserController {
                 @Autowired
                 UserService userService;
                 
+                          
                 @PostMapping("/signup")
                 public User signup(@RequestBody User user) {  
                                 logger.debug("Signing up, username: {}", user.getUsername());
                                 return this.userService.signup(user);
                 }
+                
+                @PostMapping("/login")
+                public ResponseEntity<User> login(@RequestBody User user) {
+                	
+                	logger.debug("Authenticating, username: {}, password: {}", user.getUsername(), user.getPassword());
+                		
+                	/* Spring Security Authentication. */
+                	user = this.userService.authenticate(user);
+
+                	/* Generate JWT and HTTP Header */
+                	HttpHeaders jwtHeader = this.userService.generateJwtHeader(user.getUsername());
+                				
+                	logger.debug("User Authenticated, username: {}", user.getUsername());
+                		
+                	return new ResponseEntity<>(user, jwtHeader, OK);
+                }   
+                
+                
                 @GetMapping("/test")
                 public String testController() {
                                 logger.debug("The testController() method was invoked!");
