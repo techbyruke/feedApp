5b4ed79b9516b5f9ef08d6fd53fc32365c5c156a
techbyruke <techbyruke@gmail.com>
2023-03-23 09:48:05 -0400

    commit after FeedController.java

---
+++ src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -0,0 +1,33 @@
+package com.bptn.feedApp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bptn.feedApp.jpa.Feed;
+import com.bptn.feedApp.service.FeedService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/feeds")
+public class FeedController {
+
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	FeedService feedService;
+
+	@PostMapping
+	public Feed createFeed(@RequestBody Feed feed) {
+
+		logger.debug("Creating Feed");
+
+		return this.feedService.createFeed(feed);
+	}
+
+}
