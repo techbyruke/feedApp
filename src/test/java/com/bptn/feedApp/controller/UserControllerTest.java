af8f2994f6b4e5fc56fe902804e59e3d96d4ae00
techbyruke <techbyruke@gmail.com>
2023-03-24 10:00:09 -0400

    commit after UserControllerTest.java

--- src/test/java/com/bptn/feedApp/controller/UserControllerTest.java
+++ src/test/java/com/bptn/feedApp/controller/UserControllerTest.java
@@ -24,13 +24,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.repository.UserRepository;
 import com.bptn.feedApp.security.JwtService;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -42,40 +43,38 @@ public class UserControllerTest {
 	User user;
 	String otherUsername;
 	String otherPassword;
-	
+
 	@Autowired
 	MockMvc mockMvc;
 
 	@Autowired
 	ObjectMapper objectMapper;
-	     
+
 	@Autowired
 	JwtService jwtService;
-		
+
 	@Autowired
 	UserRepository userRepository;
-	    
+
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
-	
+
 	@BeforeEach
 	public void setup() {
 
 		this.user = new User();
-		    
+
 		this.user.setFirstName("John");
 		this.user.setLastName("Doe");
 		this.user.setUsername("johndoe");
 		this.user.setPassword("mypassword");
 		this.user.setPhone("987654321");
 		this.user.setEmailId("johndoe@example.com");
-		    
+
 		this.otherUsername = "janedoe";
 		this.otherPassword = "letmein";
 	}
-	
-	
+
 	@Test
 	@Order(1)
 	public void signupIntegrationTest() throws Exception {
@@ -83,16 +82,16 @@ public class UserControllerTest {
 		ObjectMapper objectMapper = JsonMapper.builder().disable(MapperFeature.USE_ANNOTATIONS).build();
 
 		/* Check the Rest End Point Response */
-		this.mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
-	         .contentType(MediaType.APPLICATION_JSON)
-			 .content(objectMapper.writeValueAsString(user)))
-					.andExpect(status().isOk()).andExpect(jsonPath("$.firstName", is(this.user.getFirstName())))
-					.andExpect(jsonPath("$.lastName", is(this.user.getLastName())))
-					.andExpect(jsonPath("$.username", is(this.user.getUsername())))
-					.andExpect(jsonPath("$.phone", is(this.user.getPhone())))
-					.andExpect(jsonPath("$.emailId", is(this.user.getEmailId())));
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/user/signup").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(user)))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.firstName", is(this.user.getFirstName())))
+				.andExpect(jsonPath("$.lastName", is(this.user.getLastName())))
+				.andExpect(jsonPath("$.username", is(this.user.getUsername())))
+				.andExpect(jsonPath("$.phone", is(this.user.getPhone())))
+				.andExpect(jsonPath("$.emailId", is(this.user.getEmailId())));
 
-			/* Check the DB */
+		/* Check the DB */
 		Optional<User> opt = this.userRepository.findByUsername(this.user.getUsername());
 
 		assertTrue(opt.isPresent(), "User Should Exist");
@@ -107,7 +106,7 @@ public class UserControllerTest {
 
 		assertTrue(this.passwordEncoder.matches(this.user.getPassword(), opt.get().getPassword()));
 	}
-	
+
 	@Test
 	@Order(2)
 	public void signupUsernameExistsIntegrationTest() throws Exception {
@@ -115,34 +114,75 @@ public class UserControllerTest {
 		ObjectMapper objectMapper = JsonMapper.builder().disable(MapperFeature.USE_ANNOTATIONS).build();
 
 		/* Check the Rest End Point Response */
-		this.mockMvc.perform(MockMvcRequestBuilders.post("/user/signup") 
-	         .contentType(MediaType.APPLICATION_JSON)
-			 .content(objectMapper.writeValueAsString(this.user)))
-			 .andExpect(status().is4xxClientError())
-	         .andExpect(jsonPath("$.httpStatusCode", is(400)))
-			 .andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")))
-			 .andExpect(jsonPath("$.reason", is("BAD REQUEST")))
-	          .andExpect(jsonPath("$.message",
-			  is(String.format("Username already exists, %s", this.user.getUsername()))));
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/user/signup").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(this.user)))
+				.andExpect(status().is4xxClientError()).andExpect(jsonPath("$.httpStatusCode", is(400)))
+				.andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")))
+				.andExpect(jsonPath("$.reason", is("BAD REQUEST"))).andExpect(jsonPath("$.message",
+						is(String.format("Username already exists, %s", this.user.getUsername()))));
 	}
-	
+
 	@Test
 	@Order(3)
 	public void signupEmailExistsIntegrationTest() throws Exception {
-	    	
-	    ObjectMapper objectMapper = JsonMapper.builder().disable(MapperFeature.USE_ANNOTATIONS).build();
-	    	
-	    this.user.setUsername(this.otherUsername);
-	    	
-	    /* Check the Rest End Point Response */
-	    this.mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
-	    			.contentType(MediaType.APPLICATION_JSON)
-	    			.content(objectMapper.writeValueAsString(this.user)))
-	    	    .andExpect(status().is4xxClientError())
-	    	    .andExpect(jsonPath("$.httpStatusCode", is(400)))
-	    	    .andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")))
-	    	    .andExpect(jsonPath("$.reason", is("BAD REQUEST")))
-	    	    .andExpect(jsonPath("$.message", is(String.format("Email already exists, %s", this.user.getEmailId()))));
+
+		ObjectMapper objectMapper = JsonMapper.builder().disable(MapperFeature.USE_ANNOTATIONS).build();
+
+		this.user.setUsername(this.otherUsername);
+
+		/* Check the Rest End Point Response */
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/user/signup").contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(this.user)))
+				.andExpect(status().is4xxClientError()).andExpect(jsonPath("$.httpStatusCode", is(400)))
+				.andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")))
+				.andExpect(jsonPath("$.reason", is("BAD REQUEST"))).andExpect(
+						jsonPath("$.message", is(String.format("Email already exists, %s", this.user.getEmailId()))));
+	}
+
+	@Test
+	@Order(4)
+	public void verifyEmailIntegrationTest() throws Exception {
+
+		/* Check if the User exists */
+		Optional<User> opt = this.userRepository.findByUsername(this.user.getUsername());
+		assertTrue(opt.isPresent(), "User Should Exist");
+
+		/* Check the user emailVerified flag initial value */
+		assertEquals(false, opt.get().getEmailVerified());
+
+		String jwt = String.format("Bearer %s", this.jwtService.generateJwtToken(this.user.getUsername(), 10_000));
+
+		/* Check the Rest End Point Response */
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/user/verify/email").header(AUTHORIZATION, jwt))
+				.andExpect(status().isOk());
+
+		/* Check if the User exists */
+		opt = this.userRepository.findByUsername(this.user.getUsername());
+		assertTrue(opt.isPresent(), "User Should Exist");
+
+		/* Check the user emailVerified flag was updated */
+		assertEquals(true, opt.get().getEmailVerified());
+
+	}
+
+	@Test
+	@Order(5)
+	public void verifyEmailUsernameNotFoundIntegrationTest() throws Exception {
+
+		/* Check if the User exists */
+		Optional<User> opt = this.userRepository.findByUsername(this.otherUsername);
+		assertTrue(opt.isEmpty(), "User Should Not Exist");
+
+		String jwt = String.format("Bearer %s", this.jwtService.generateJwtToken(this.otherUsername, 10_000));
+
+		/* Check the Rest End Point Response */		this.mockMvc.perform(MockMvcRequestBuilders.get("/user/verify/email").header(AUTHORIZATION, jwt))
+	.andExpect(status().is4xxClientError()).andExpect(jsonPath("$.httpStatusCode", is(400)))
+					.andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")))
+					.andExpect(jsonPath("$.reason", is("BAD REQUEST")))
+					.andExpect(jsonPath("$.message", is(String.format("Username doesn't exist, %s", this.otherUsername))));
+
 	}
 	
 }
