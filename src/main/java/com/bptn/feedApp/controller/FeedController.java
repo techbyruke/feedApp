144c448171552a69db92240269a1990d40ea62c7
techbyruke <techbyruke@gmail.com>
2023-03-24 08:30:35 -0400

    commit after FeedController.java

--- src/main/java/com/bptn/feedApp/controller/FeedController.java
+++ src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bptn.feedApp.domain.PageResponse;
 import com.bptn.feedApp.jpa.Feed;
+import com.bptn.feedApp.jpa.FeedMetaData;
 import com.bptn.feedApp.service.FeedService;
 
 @CrossOrigin
@@ -55,6 +56,14 @@ public class FeedController {
 		logger.debug("Creating Feed");
 
 		return this.feedService.createFeed(feed);
+	}
+	
+	@PostMapping("/meta/{feedId}")
+	public FeedMetaData createFeedMetaData(@PathVariable int feedId, @RequestBody FeedMetaData meta) {
+			
+		logger.debug("Creating FeedMetaData, feedId: {}", feedId);
+			
+		return this.feedService.createFeedMetaData(feedId, meta);
 	}
 
 }
